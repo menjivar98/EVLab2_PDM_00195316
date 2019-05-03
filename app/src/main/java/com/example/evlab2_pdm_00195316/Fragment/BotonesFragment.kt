f597b1f014ff629f6a2ed3bcdce3e73ebc98fc0c
marielosagui <00078913@uca.edu.sv>
2019-05-03 08:43:56 -0600

    Terminando el diseño del laboratorio

--- app/src/main/java/com/example/evlab2_pdm_00195316/Fragment/BotonesFragment.kt
+++ app/src/main/java/com/example/evlab2_pdm_00195316/Fragment/BotonesFragment.kt
@@ -7,8 +7,13 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.evlab2_pdm_00195316.R
+import kotlinx.android.synthetic.main.fragment_botones.*
 
 class BotonesFragment : Fragment() {
+
+
+   var btn1 = btn_despues
+    var btn2 = btn_anterior
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -17,5 +22,9 @@ class BotonesFragment : Fragment() {
         return view
 
     }
+
+
+
+
 
 }
